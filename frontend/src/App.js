ed443d1073eba64f17786a9b807a6536a3d75318
Ishan Samintha <isamintha@Ishans-Mac-mini.local>
2023-03-14 14:31:07 +0530

    Initial update

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amzona</a>
+      </header>
+      <main>Product list</main>
+    </div>
+  );
+}
+
+export default App;
